89abaf12e2d4461ca10e564c2a995429abccb1f9
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-09-14 06:50:55 -0600

    removed unused files

--- helpers.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-logger.propagate = True  # important: send logs to root logger
-
-def my_helper_function():
-    logger.info("Hello from helper function!")
